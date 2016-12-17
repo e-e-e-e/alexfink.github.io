5c33187948d2016a7e1753b311d45efc8716d194
Herdnaxy Hitni <herdnaxy@gmail.com>
2016-12-17 14:31:07 +0000

    Update game_manager.js

--- dive/js/game_manager.js
+++ dive/js/game_manager.js
@@ -26,9 +26,9 @@ GameManager.prototype.setup = function () {
   this.gameMode     = +(select.options[select.selectedIndex].value);
   this.tileTypes = [2,3,5,7];
   if (this.gameMode & 1) {
-    this.tileTypes = [111];
+    this.tileTypes = [769];
     this.actuator.updateCurrentlyUnlocked(this.tileTypes);
-    this.tilesSeen = [111];
+    this.tilesSeen = [769];
   } 
 
   this.score        = 0;
